49f68ed0947f4c3d4ad4a5cef62cc4bea511afca
EverVargash <ebvargash@unjbg.edu.pe>
2025-05-30 15:00:36 -0500

    Mejorando el archivo funcion03.cpp - el usuario podra hacer la operacion cuantas veces lo desee

--- funcion03.cpp
+++ funcion03.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int factorial(int a);
 
 int main(){
+
     int c, num1, num2;
-
-    cout<<"            Combinatoria m sobre n"<<endl;
-    cout<<"Ingrese el valor de m: "; cin>>num1;
-    cout<<"Ingese el valor de n: "; cin>>num2;
-
-    c=factorial(num1)/(factorial(num2)*factorial(num1 - num2));
-    cout<<"La combinatoria de m sobre n es: "<<c;
+    string op;
+    do{   
+        cout<<"                  Combinatoria m sobre n"<<endl;
+        cout<<"Ingrese el valor de m: "; cin>>num1;
+        cout<<"Ingese el valor de n: "; cin>>num2;
+    
+        if(num1>0 && num2>0 && num1>num2){
+            c=factorial(num1)/(factorial(num2)*factorial(num1 - num2));
+            cout<<"La combinatoria de "<<num1<<" sobre "<<num2<<" es: "<<c<<endl;
+         }
+        else cout<<"Ingrese numeros positivos donde m tiene que ser mayor a n"<<endl;
+        cout<<"Ingrese si para intentar de nuevo: "; cin>>op;
+    } while (op=="si");
+    
     return 0;
 }
 
