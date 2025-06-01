cfaf4fe7f972587ffac27db78d07071c549c2582
EverVargash <ebvargash@unjbg.edu.pe>
2025-06-01 15:03:01 -0500

    Primera version del archivo funcion06.cpp (calcular el resto y cociente con restas)

---
+++ funcion06.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void resto(int a, int b);
+
+int main(){
+    int num, dnm;
+    
+    cout<<"            CALCULAR RESTO Y COCIENTE CON RESTAS"<<endl;
+    cout<<"Ingese el numerador: "; cin>>num;
+    cout<<"Ingrese el denominador: "; cin>>dnm;
+
+    resto(num, dnm);
+    return 0;
+}
+
+void resto(int a, int b){
+    int resto, cant=0, rpt;
+    while(a>=b){
+        a-=b;
+        cant+=1;
+    }
+    cout<<"El resto es: "<<a<<endl;
+    cout<<"El cociente es: "<<cant<<endl;
+}
