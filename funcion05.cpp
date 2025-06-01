30a6a000313cd88f7d2a3ed7c45648b498924df5
EverVargash <ebvargash@unjbg.edu.pe>
2025-06-01 13:42:51 -0500

    Mejorando archivo funcion05.cpp - creando una funcion que calcule el mcd de dos numeros, tambien el usurio vera si los numeros ingresesados son validos

--- funcion05.cpp
+++ funcion05.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 using namespace std;
 
+int maxcd(int a, int b);
+
 int main(){
-    int num1, num2, mcd, mcm;
+    int num1, num2, mcm;
     
     cout<<"                    CALCULAR EL MCM Y MCD"<<endl;
     cout<<"Ingrese un numero: "; cin>>num1;
     cout<<"Ingrese otro numero: "; cin>>num2;
 
-    for(int i=1 ; i<=num1 && i<=num2 ; i++){
-        if(0==num1%i && 0==num2%i){
+    if(num1>0 && num2>0 ){
+        cout<<"El MCD de "<<num1<<" y "<<num2<<" es: "<<maxcd(num1, num2)<<endl;
+        mcm=num1*num2/maxcd(num1, num2);
+        cout<<"El MCM de "<<num1<<" y "<<num2<<" es: "<<mcm<<endl;
+    }
+    else cout<<"Los numeros tienen que ser enteros positivos";
+    return 0;
+}
+
+int maxcd(int a, int b){
+    int mcd;
+    for(int i=1 ; i<=a && i<=b ; i++){
+        if(0==a%i && 0==b%i){
             mcd=i;
         }
     }
-    mcm=num1*num2/mcd;
-    cout<<"El MCD es: "<<mcd<<endl;
-    cout<<"El MCM es: "<<mcm<<endl;
-    return 0;
+    return mcd;
 }
