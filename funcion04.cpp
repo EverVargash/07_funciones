41dbd6acb6979c83f17afcfc9beff56ecbb3a525
EverVargash <ebvargash@unjbg.edu.pe>
2025-05-31 22:01:20 -0500

    Mejorando el archivo funcion04.cpp (Ahora el usuario podra decidir si continuar con el programa si ingresa un numero erroneo)

--- funcion04.cpp
+++ funcion04.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
     int num, cdv=0;
-    cout<<"               VERIFICAR SI UN NUMERO ES PRIMO"<<endl;
-    cout<<"Ingrese el numero: "; cin>>num;
+    string op;
+    cout<<"                 VERIFICAR SI UN NUMERO ES PRIMO"<<endl;
+    do {
+        cout<<"Ingrese el numero: "; cin>>num;
 
-    for(int i=1 ; i<=num ; i++){
-        if(0==num%i){
-            cdv+=1;
+        if(num>0){
+            for(int i=1 ; i<=num ; i++){
+                if(0==num%i){
+                    cdv+=1;
+                }
+            }
+            if(cdv==2){
+                cout<<"El numero si es primo"<<endl;
+            }
+            else cout<<"El numero no es primo"<<endl;
         }
-    }
-    if(cdv==1){
-        cout<<"El numero si es primo";
-    }
-    else cout<<"El numero no es primo";
+
+        else {
+            cout<<"ingrese un numero mayor a 0"<<endl;
+            cout<<"Presione y para continuar o n para salir"<<endl; cin>>op;
+        }
+    } while(op=="y");
+
     return 0;
+
 }
