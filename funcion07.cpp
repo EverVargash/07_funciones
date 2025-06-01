aae8a4fbebc5f71508a81f3b6866103b08ad3452
EverVargash <ebvargash@unjbg.edu.pe>
2025-06-01 15:56:02 -0500

    Mejorando el archivo funcion07.cpp - ahora el usurio podra ver el erro al momento de ingresar un numero y podra volver a intentarlo

--- funcion07.cpp
+++ funcion07.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
+#include <iostream>
 using namespace std;
 
 int div(int a);
 
 int main(){
     int num;
-    cout<<"         VERIFICAR SI UN NUMERO ES PERFECTO"<<endl;
-    cout<<"Ingrese un numero entero: "; cin>>num;
-    if(div(num)==num) cout<<"El numero es perfecto";
-    else cout<<"El numero no es perfecto";
+    string op;
+    cout<<"*****************************************************************"<<endl;
+    cout<<"**************VERIFICAR SI UN NUMERO ES PERFECTO*****************"<<endl;
+    cout<<"*****************************************************************"<<endl;
+    do{
+        cout<<"Ingrese un numero entero: "; cin>>num;
+        if(num>0){
+            if(div(num)==num){
+            cout<<"El numero "<<num<<" es perfecto"<<endl;
+            }
+        }
+        else cout<<"Ingrese un numero entero positivo"<<endl;
+        cout<<"Presione cualquier otra tecla para salir"<<endl;
+        cout<<"Presione y para intentar nuevamente: "; cin>>op;
+        
+    } while(op=="y");
     return 0;
 }
 
