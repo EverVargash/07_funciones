7570f64bb6e18a8c0fee7a223ae30e1c516f1edf
EverVargash <ebvargash@unjbg.edu.pe>
2025-06-01 15:12:02 -0500

    Mejorando el archivo funcion06.cpp - ahora el usuario debera poner un denominar diferente de cero

--- funcion06.cpp
+++ funcion06.cpp
@@ -5,12 +5,16 @@ void resto(int a, int b);
 
 int main(){
     int num, dnm;
-    
+    cout<<"******************************************************************"<<endl;
     cout<<"            CALCULAR RESTO Y COCIENTE CON RESTAS"<<endl;
+    cout<<"******************************************************************"<<endl;
     cout<<"Ingese el numerador: "; cin>>num;
     cout<<"Ingrese el denominador: "; cin>>dnm;
 
-    resto(num, dnm);
+    if(dnm != 0){
+        resto(num, dnm);
+    }
+    else cout<<"El denominador no puede ser cero";
     return 0;
 }
 
